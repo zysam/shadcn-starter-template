93de8cbdbc89258bb08d8cc7df3d29fc31c09526
zysam <explnwind@gmail.com>
2024-11-27 11:53:22 +0800

    Update docs/readme.md, remove pnpm-lock.yaml, rename prettier.config.js to prettier.config.cjs, and update src/index.css and vite.config.ts

--- docs/readme.md
+++ docs/readme.md
@@ -1,4 +1,1 @@
-# quick-template-02
-
-一个快速开发模板，基于 vite + react + tailwindcss + shadcn-ui + typescript
-
+# shadcn-stater-template
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-// import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  // resolve: {
-  //   alias: {
-  //     '@': path.resolve(__dirname, './src'),
-  //   },
-  // },
 });
